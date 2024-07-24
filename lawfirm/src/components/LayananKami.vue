e7200e40715d53e4b2902e5eb34f2ee75e9f701d
Muchammad Nur Rizal Pangestu <muchammadnurrizalpangestu@Muchammads-MacBook-Pro.local>
2024-07-24 14:46:38 +0700

    adjust

--- lawfirm/src/components/LayananKami.vue
+++ lawfirm/src/components/LayananKami.vue
@@ -7,155 +7,91 @@ import Dropdown from './DetailsDropdown.vue'
     <div class="about">
         <h2 class="text-2xl font-semibold mt-10 ml-28">Layanan Kami</h2>
         <p class="text-5xl font-semibold ml-28 mt-4">Lorem ipsum dolor sit amet,
-             <p class="text-5xl font-semibold ml-28">consectetur adipiscing elit.</p> </p>
+        <p class="text-5xl font-semibold ml-28">consectetur adipiscing elit.</p>
+        </p>
     </div>
 
-     <div class="mt-20 justify-center ml-12">
+    <div class="mt-20 justify-center ml-12">
         <img src="../assets/img/coba.png" class="">
-     </div>
-
-     <div>
-
-    <div class="text-xl mt-20 ml-20 mr-10 flex">
-      <!-- Konten 1 -->
-      <div class="text-xl mt-auto">
-        Lorem ipsum dolor sit amet, adipiscing elit.
-        <p>Nullam fringilla nec nisl id vehicula. Curabitur leo ante,</p>
-        <p>ornare ac urna sodales, volutpat egestas ante.</p>
-        <p>Praesent volutpat turpis nibh, vitae posuere erat elementum nec.</p>
-      </div>
-
-      <!-- Konten 2 -->
-      <div class="text-xl mt-auto ml-56 mr-10">
-        Lorem ipsum dolor sit amet, adipiscing elit.
-        <p>Nullam fringilla nec nisl id vehicula. Curabitur leo ante,</p>
-        <p>ornare ac urna sodales, volutpat egestas ante.</p>
-        <p>Praesent volutpat turpis nibh, vitae posuere erat elementum nec.</p>
-      </div>
     </div>
 
-    <div class="mt-8 ml-20">
-      <div class="bg-blue-900 w-40 py-3 rounded-full hover:bg-white border-2 border-white text-center">
-        <button class="font-bold text-white">LIHAT LEGALITAS</button>
-      </div>
-    </div>
-  </div>
-     
-<div class="layanan my-10">
-    <p class="text-3xl font-bold ml-56 mt-20">SEMUA LAYANAN</p>
-    <Dropdown title="Litgasi dan penyelesaian sengketa" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-    <Dropdown title="Non-Litgasi" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-    <Dropdown title="Korporasi dan Komersial" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-    <Dropdown title="Hukum Perdata" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-    <Dropdown title="Hukum Perburuhan" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-    <Dropdown title="Hukum Properti" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-    <Dropdown title="Hukum Keluarga" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-    <Dropdown title="Hukum Pidana" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-    <Dropdown title="Asisten Pajak" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-    <Dropdown title="Perizinan Usaha" class="cursor-pointer text-2xl font-semibold ml-56 mt-8 mr-60">
-        <div class="text-xl ml-16 mt-3 mr-60 my-3 font-normal"> 
-             <ul class="list-disc list-inside">
-                 <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-             </ul>
-        </div>
-    </Dropdown>
-    <hr>
-</div>
+    <div>
 
-<div class="relative mt-20">
-    <!-- Gambar -->
-    <img src="../assets/img/coklat.png" class="w-full h-auto">
-    <div class="absolute inset-0 flex mt-52 ml-40 my-4">
-        <div v-show="isVisible" class="animate-slideInFromLeft">
-            <span class="text-biru_tengah text-2xl">Langkah pertama <br></span>
-      <span class="text-biru_tengah text-4xl ">Lorem ipsum dolor sit amet,
-         consectetur <br> adipiscing elit.</span>
-    </div>
-  </div>
-</div>
+        <div class="text-xl mt-20 ml-20 mr-10 flex">
+            <!-- Konten 1 -->
+            <div class="text-xl mt-auto">
+                Lorem ipsum dolor sit amet, adipiscing elit.
+                <p>Nullam fringilla nec nisl id vehicula. Curabitur leo ante,</p>
+                <p>ornare ac urna sodales, volutpat egestas ante.</p>
+                <p>Praesent volutpat turpis nibh, vitae posuere erat elementum nec.</p>
+            </div>
 
+            <!-- Konten 2 -->
+            <div class="text-xl mt-auto ml-56 mr-10">
+                Lorem ipsum dolor sit amet, adipiscing elit.
+                <p>Nullam fringilla nec nisl id vehicula. Curabitur leo ante,</p>
+                <p>ornare ac urna sodales, volutpat egestas ante.</p>
+                <p>Praesent volutpat turpis nibh, vitae posuere erat elementum nec.</p>
+            </div>
+        </div>
+
+        <div class="mt-8 ml-20">
+            <div class="bg-blue-900 w-40 py-3 rounded-full hover:bg-white border-2 border-white text-center">
+                <button class="font-bold text-white">LIHAT LEGALITAS</button>
+            </div>
+        </div>
+    </div>
+
+    <div class="layanan my-10 px-24">
+        <p class="text-3xl font-bold mt-20">SEMUA LAYANAN</p>
+        <Dropdown title="Litgasi dan penyelesaian sengketa"
+            class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+        <Dropdown title="Non-Litgasi" class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+        <Dropdown title="Korporasi dan Komersial" class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+        <Dropdown title="Hukum Perdata" class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+        <Dropdown title="Hukum Perburuhan" class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+        <Dropdown title="Hukum Properti" class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+        <Dropdown title="Hukum Keluarga" class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+        <Dropdown title="Hukum Pidana" class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+        <Dropdown title="Asisten Pajak" class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+        <Dropdown title="Perizinan Usaha" class="cursor-pointer text-2xl">
+            <li class="text-sm ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
+        </Dropdown>
+        <hr>
+    </div>
 </template>
 
 
 <script>
 export default {
-    name: '',
-    data() {
-        return {
-            isVisible: false
-        }
-    },
-    mounted() {
-        this.isVisible = true;
-    }
-
-    
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
