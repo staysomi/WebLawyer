86b9bf2864fc1c8ebc3d24c43e338dfadf5ee423
nalinikarina <nalinikarina@student.ub.ac.id>
2024-07-27 15:01:25 +0700

    layanan kami

--- lawfirm/src/components/LayananKami.vue
+++ lawfirm/src/components/LayananKami.vue
@@ -18,9 +18,9 @@ import Popup from './PopupBio.vue'
 
     <div>
 
-        <div class="text-sm mt-20 flex justify-center">
+        <div class="text-sm mt-20 flex flex-col md:flex-row">
             <!-- Konten 1 -->
-            <div class="md:text-xl p-4 ">
+            <div class="md:text-xl p-4 md:ml-28 ">
                 Lorem ipsum dolor sit amet, adipiscing elit.
                 <p>Nullam fringilla nec nisl id vehicula. Curabitur leo ante,</p>
                 <p>ornare ac urna sodales, volutpat egestas ante.</p>
@@ -30,21 +30,21 @@ import Popup from './PopupBio.vue'
             </div>
 
             <!-- Konten 2 -->
-            <div class="md:text-xl p-4 hidden md:block">
-                Lorem ipsum dolor sit amet, adipiscing elit.
-                <p>Nullam fringilla nec nisl id vehicula. Curabitur leo ante,</p>
-                <p>ornare ac urna sodales, volutpat egestas ante.</p>
-                <p>Praesent volutpat turpis nibh, vitae posuere erat elementum.</p>
-            </div>
-        </div>
+            <div class="md:text-xl p-4 block md:mr-28 md:ml-10">
+        Lorem ipsum dolor sit amet, adipiscing elit.
+        <p>Nullam fringilla nec nisl id vehicula. Curabitur leo ante,</p>
+        <p>ornare ac urna sodales, volutpat egestas ante.</p>
+        <p>Praesent volutpat turpis nibh, vitae posuere erat elementum.</p>
         <div @click="showPopup = true"
-            class="cursor-pointer bg-blue-900 w-40 py-1 rounded-full hover:bg-blue-300 border-2 border-white text-center text-white md:text-l md:ml-44 mt-4">
-            LIHAT LEGALITAS</div>
+            class="cursor-pointer bg-blue-900 w-40 py-1 rounded-full hover:bg-blue-300 border-2 border-white text-center text-white md:text-sm mt-4">
+            LIHAT LEGALITAS
+        </div>
+     </div>
+    </div>
     </div>
 
-
-    <div class="layanan my-4 px-4 md:ml-40 md:mr-40 md:mb-20">
-        <p class="md:text-3xl font-bold mt-20">SEMUA LAYANAN</p>
+    <div class="layanan my-4 px-4 md:ml-28 md:mr-40 mb-10">
+        <p class="md:text-3xl font-bold mt-10 md:mt-20">SEMUA LAYANAN</p>
         <Dropdown title="Litgasi dan penyelesaian sengketa" class="cursor-pointer md:text-xl md:mt-4">
             <li class="md:text-medium ml-8"> ipsum dolor sit amet, consectetur adipiscing elit.</li>
         </Dropdown>
--- lawfirm/src/components/TimKami.vue
+++ lawfirm/src/components/TimKami.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="section-1">
-        <h2 class="md:text-2xl font-semibold md:mt-10 md:ml-28">Tim Kami</h2>
-        <p class="md:text-4xl font-semibold mt-4 md:ml-28">INOVASI DAN KOLABORASI <br>DALAM PERSPEKTIF HUKUM</p>
+        <h2 class="md:text-2xl font-semibold mt-5 md:ml-28 ml-4">Tim Kami</h2>
+        <p class="md:text-4xl font-semibold mt-4 ml-4 mb-5 md:ml-28">INOVASI DAN KOLABORASI <br>DALAM PERSPEKTIF HUKUM</p>
         <img src="https://placehold.co/600x400" alt="" class="w-full md:h-60 md:mt-20">
 
         <div class="md:text-xl md:mt-20 p-4 md:ml-28 md:mr-10 md:flex md:justify-start md:items-start">
@@ -23,39 +23,39 @@
     </div>
 
 
-    <div class="section-profile">
-        <p class="md:text-2xl font-semibold md:mt-10 md:ml-28">
+    <div class="section-profile ">
+        <p class="md:text-2xl font-semibold mt-8 ml-4 md:ml-28">
             SEMUA PROFILE
         </p>
-        <p class="md:ml-28 mt-4 md:text-xl">Tim kami terdiri dari advokat berpengalaman dan ahli yang terus mengembangkan <br>
+        <p class="ml-4 mt-4 md:text-xl md:ml-28">Tim kami terdiri dari advokat berpengalaman dan ahli yang terus mengembangkan <br>
             keterampilan melalui pelatihan berkelanjutan. </p>
             <div class="flex justify-around md:mt-10">
-    <!-- Konten pertama -->
-    <div class="flex w-full flex-col justify-center items-center">
-        <img src="../assets/img/tim11x1.png" alt="Slide Image" class="md:w-auto md:h-96 mt-6" />
-        <p class="md:text-lg">Advokat</p>
-        <p class="md:text-xl">Muchamad Ilham Tantowi. S.H., M.H</p>
-        <router-link to="tim1">
-            <div
-                class="cursor-pointer mt-4 py-2 px-4 w-fit h-fit text-sm bg-transparent border border-gray-400 rounded-full text-gray-400">
-                Lihat Bio
-            </div>
-        </router-link>
-    </div>
-
-    <!-- Konten kedua -->
-    <div class="flex w-full flex-col justify-center items-center">
-        <img src="../assets/img/tim21x1.png" alt="Slide Image" class="md:w-auto md:h-96 md:mt-20" />
-        <p class="md:text-lg">Advokat</p>
-        <p class="md:text-xl">Pak Arif</p>
-        <router-link to="tim2">
-            <div
-                class="cursor-pointer mt-4 py-2 px-4 w-fit h-fit text-sm bg-transparent border border-gray-400 rounded-full text-gray-400 md:mb-20">
-                Lihat Bio
-            </div>
-        </router-link>
-    </div>
+   <!-- Konten pertama -->
+<div class="flex w-full flex-col justify-center items-center pb-10">
+    <img src="../assets/img/tim11x1.png" alt="Slide Image" class="md:w-auto md:h-96 mt-6" />
+    <p class="md:text-lg">Advokat</p>
+    <p class="md:text-xl text-center">Muchamad Ilham Tantowi. S.H., M.H</p>
+    <router-link to="tim1">
+        <div
+            class="cursor-pointer mt-4 py-2 px-4 w-fit h-fit text-sm bg-transparent border border-gray-400 rounded-full text-gray-400">
+            Lihat Bio
+        </div>
+    </router-link>
 </div>
+
+<!-- Konten kedua -->
+<div class="flex w-full flex-col justify-center items-center pb-10">
+    <img src="../assets/img/tim21x1.png" alt="Slide Image" class="md:w-auto md:h-96" />
+    <p class="md:text-lg">Advokat</p>
+    <p class="md:text-xl">Pak Arif</p>
+    <router-link to="tim2">
+        <div
+            class="cursor-pointer mt-4 py-2 px-4 w-fit h-fit text-sm bg-transparent border border-gray-400 rounded-full text-gray-400">
+            Lihat Bio
+        </div>
+    </router-link>
+</div>
+            </div>
     </div>
 </template>
 
